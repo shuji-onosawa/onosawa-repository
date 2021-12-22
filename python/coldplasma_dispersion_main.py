b6750eae5b45d5725cd9a02de906a2f0f8d03795
onosawa <shuji.onosawa.p8@dc.tohoku.ac.jp>
2021-12-22 16:11:03 +0900

    共鳴エネルギー計算

--- python/coldplasma_dispersion_main.py
+++ python/coldplasma_dispersion_main.py
@@ -32,10 +32,11 @@ def dispersion(theta, w):
     A = S*(np.sin(theta))**2 + P*(np.cos(theta))**2
     B = R*L*(np.sin(theta))**2 + P*S*(1+(np.cos(theta))**2)
     C = P*R*L
-    F = (B**2 - 4*A*C)**0.5
+    F = np.sqrt(B**2 - 4*A*C)
 
     n1 = (B + F)/(2*A)
     n2 = (B - F)/(2*A)
+    
     for i in range(w.size):
         if n1[i] < 0:
             n1[i] = np.nan
@@ -43,20 +44,37 @@ def dispersion(theta, w):
         if n2[i] < 0:
             n2[i] = np.nan
     
-    G = B**2 - 4*A*C
-    for i in range(w.size):
-        if G[i] < 0:
-            n1[i], n2[i] = n2[i], n1[i]
-    
-    n1 = n1**0.5
-    n2 = n2**0.5
-
-    k1, k2 = w/c*n1, w/c*n2
+    k1, k2 = w/c*np.sqrt(n1), w/c*np.sqrt(n2)
     return k1,k2
 
 theta = 0
+alpha = 80
+w = abs(omega_o)*np.arange(0.01, 2, 0.01) + 0.1
+k1, k2 = dispersion(theta, w)
+
+v = (w + omega_o)/k2/np.cos(np.radians(alpha - theta))
+Eo = mo*v**2/2
+cm = plt.cm.get_cmap('RdYlBu')
+
+plt.figure()
+plt.scatter(k1, w/abs(omega_o), c=Eo, cmap = cm, label=r'$k+$')
+plt.scatter(k2, w/abs(omega_o), c=Eo, cmap = cm, label=r'$k-$')
+#plt.plot(w/Va, w/abs(omega_o), linestyle='dashed')
+#plt.hlines(omega_e/omega_o, 0, 0.002, colors='black', linestyles='dashed')
+#plt.hlines(wlh/abs(omega_o), 0, 1, colors='black', linestyles='dashed')
+plt.xscale('log')
+plt.xlabel('k [/m]')
+plt.ylabel(r'$\omega/\Omega_o$')
+plt.colorbar(label='Energy [eV]')
+plt.legend()
+plt.show()
+
 w = abs(omega_e)*np.arange(0.01, 10, 0.01) + 0.1
 k1, k2 = dispersion(theta, w)
+
+v = (w + omega_o)/k2/np.cos(np.radians(alpha - theta))
+Eo = mo*v**2/2
+cm = plt.cm.get_cmap('RdYlBu')
 
 Va = B0*c*eps**0.5/(mo*n)**0.5
 wuh = (omega_e**2 + pi_e**2)**0.5
@@ -70,20 +88,5 @@ plt.hlines(wuh/abs(omega_e), 0, 1, colors='black', linestyles='dashed')
 plt.xscale('log')
 plt.xlabel('k [/m]')
 plt.ylabel(r'$\omega/\Omega_e$')
-plt.legend()
-plt.show()
-
-w = abs(omega_o)*np.arange(0.01, 30000, 0.01) + 0.1
-k1, k2 = dispersion(theta, w)
-
-plt.figure()
-plt.plot(k1, w/abs(omega_o), label=r'$k+$')
-plt.plot(k2, w/abs(omega_o), label=r'$k-$')
-plt.plot(w/Va, w/abs(omega_o), linestyle='dashed')
-plt.hlines(omega_e/omega_o, 0, 0.002, colors='black', linestyles='dashed')
-#plt.hlines(wlh/abs(omega_o), 0, 1, colors='black', linestyles='dashed')
-plt.xscale('log')
-plt.xlabel('k [/m]')
-plt.ylabel(r'$\omega/\Omega_o$')
 plt.legend()
 plt.show()
